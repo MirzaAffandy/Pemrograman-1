7990c5a7f7783b0df70bfd0c47a6bc15cfebde50
unknown <mirza.afandi@ymail.com>
2013-09-28 17:47:25 +0700

    commit02

---
+++ sessi 01/src/NoKTP.java
@@ -0,0 +1,22 @@
+//Tugas 1
+package latihan02.sesi01.bin;
+
+public class NoKTP
+{
+   public static void main(String[] data){
+      System.out.println("No KTP           : 3276102605910001");
+	  System.out.println("Nama             : Mirza Afandi");
+	  System.out.println("Tempat/tgl Lahir : Depok");
+	  System.out.println("Jenis Kelamin    : Laki-Laki");
+	  System.out.println("Golongan Darah   : B");
+	  System.out.println("Alamat           : KP.BANJARAN PUCUNG");
+	  System.out.println("RT/RW            : 002/010");
+	  System.out.println("Desa/Kel         : CILANGKAP");
+	  System.out.println("Kecamatan        : TAPOS");
+	  System.out.println("Agama            : Islam");
+	  System.out.println("Pekerjaan        : Mahasiswa");
+	  System.out.println("Berlaku hingaa   : 26-05-2015");
+	  System.out.println("Kewarganegaraan  : WNI");
+	  System.out.println("Status           : Belum Menikah");
+    }
+}
