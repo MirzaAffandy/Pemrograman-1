6cbcbc32c247c0115759830f89039bfa591308e3
MirzaAffandy <mirza.afandi@ymail.com>
2013-11-09 14:46:42 +0700

    commit7

---
+++ sessi 01/src/icrement.java
@@ -0,0 +1,37 @@
+/*
+ * class icrement.
+ * Mirza afandy
+ */
+
+package latihan01.sesi1.bin;
+
+public class icrement
+{
+      public static void main(String[] args)
+    {
+
+        int a;
+        a=120;
+        System.out.println ("contoh postfix icrement :");
+		System.out.println (a);
+		System.out.println (a++);
+		System.out.println (a);
+		a=120;
+		System.out.println ("contoh prefix icrement :");
+		System.out.println (a);
+		System.out.println (a++);
+		System.out.println (a);
+		a=120;
+		System.out.println ("contoh postfix icrement :");
+        System.out.println (a);
+		System.out.println (a++);
+		System.out.println (a);
+		a=120;
+		System.out.println ("contoh prefix icrement :");
+		System.out.println (a);
+		System.out.println (a++);
+		System.out.println (a);
+	}
+}
+		
+ 
