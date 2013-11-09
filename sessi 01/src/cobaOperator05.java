7a720f0fd10329685ad3d2cf1ce274595536609c
MirzaAffandy <mirza.afandi@ymail.com>
2013-11-09 14:59:25 +0700

    commit9

---
+++ sessi 01/src/cobaOperator05.java
@@ -0,0 +1,31 @@
+//Perintah Operator
+
+package Latihan01.sesi1.bin;
+
+public class cobaOperator05
+{
+   public static void main(String[] Xx)
+   {
+      int c=3; int d=5; int e=4; float f=6; float g=12;
+	  System.out.println("Nilai -c- awal  : "+c);
+	  c+=7;
+	  System.out.println("Nilai c+=7      : "+c);
+	  System.out.println();
+	  System.out.println("Nilai -d- awal  : "+d);
+	  d-=4;
+	  System.out.println("Nilai d-=4      : "+d);
+	  System.out.println();
+	  System.out.println("Nilai -e- awal  : "+e);
+	  e*=5;
+	  System.out.println("Nilai e*=5      : "+e);
+	  System.out.println();
+	  System.out.println("Nilai -f- awal  : "+f);
+	  f/=5;
+	  System.out.println("Nilai f/=5      : "+f);
+	  System.out.println();
+	  System.out.println("Nilai -g- awal  : "+g);
+	  g%=7;
+	  System.out.println("Nilai g%=7      : "+g);
+	  System.out.println();
+	}
+}	
