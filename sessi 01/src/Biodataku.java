6b0223d96ab93373b64f3d8c77230740016a6bed
MirzaAffandy <mirza.afandi@ymail.com>
2013-11-09 15:11:53 +0700

    commit13

---
+++ sessi 01/src/Biodataku.java
@@ -0,0 +1,12 @@
+//Perintah penggunaan package version 01
+package latihan01.sesi01.bin;
+
+public class Biodataku
+{
+   public static void main(String[] data){
+      System.out.println("Nama             : Mirza Afandi");
+	  System.out.println("Alamat           : Depok");
+	  System.out.println("Golongan Darah   : B");
+	  System.out.println("Jenis Kelamin    : Laki-Laki");
+   }
+}
