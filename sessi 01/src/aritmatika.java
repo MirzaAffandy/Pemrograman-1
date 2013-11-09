a9f3626e257dc5dcde8346210165e997b15877f9
MirzaAffandy <mirza.afandi@ymail.com>
2013-11-09 15:09:45 +0700

    commit1

---
+++ sessi 01/src/aritmatika.java
@@ -0,0 +1,18 @@
+//Perintah Operator
+package latihan01.sesi1.bin;
+
+public class CobaOperator02{
+      public static void main(String[] Xx)   {
+	    int x=2012;
+		int kbt1 = x %4;
+		System.out.println ("Tahun :" +x);
+		System.out.println ("kabisat =0/Bukan kabisat =1,hasilnya 0");
+		System.out.println ();
+		System.out.println ();
+		inty=2013;
+		int kbt2 =y %4;
+		System.out.println ("contoh postfix icrement :");
+		System.out.println ("Kabisat =0/Bukan kabisat =1,hasilnya 1");
+		System.out.println ();
+	}
+}
