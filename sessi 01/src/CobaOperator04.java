d5c73011b66dcffb211f4bc9c7eca60aa2a93573
MirzaAffandy <mirza.afandi@ymail.com>
2013-11-09 14:56:58 +0700

    commit8

---
+++ sessi 01/src/CobaOperator04.java
@@ -0,0 +1,15 @@
+//Perintah Operator -- BITWISE
+
+package latihan01.sesi01.bin;
+
+public class CobaOperator04
+{
+   public static void main(String[] XX)
+   {
+   int x=100;
+   System.out.println("Nilai Binary      [bin] 100= " + Integer.parseInt("100",2));
+   System.out.println("Nilai Octal       [Oct] 100= " + Integer.parseInt("100",8));
+   System.out.println("Nilai Decimal     [Dec] 100= " + Integer.parseInt("100",10));
+   System.out.println("Nilai Hexadecimal [Hex] 100= " + Integer.parseInt("100",16));
+   }
+}
