9bd367071c83e6d62c91302f58aaf16807f5c8f4
MirzaAffandy <mirza.afandi@ymail.com>
2013-11-09 13:43:20 +0700

    commit5

---
+++ sessi 01/src/CobaOperator01.java
@@ -0,0 +1,18 @@
+//perintah Operator
+package Latihan01.sesi1.bin;
+
+public class CobaOperator01
+{
+public static void main(String[] Xx)
+{
+int x=7;
+System.out.println("Nilai awal X adalah " +x);
+System.out.println("Nilai X++ adalah "+ x++);
+System.out.println("Nilai X adalah " +x);
+System.out.println();
+int y=25;
+System.out.println("Nilai awal Y adalah " +y);
+System.out.println("Nilai Y++ adalah " + ++y);
+System.out.println("Nilai X adalah " +y);
+}
+}
